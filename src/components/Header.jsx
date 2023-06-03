c4109092baccd0074a84e206fb26fe2bd984d144
Asur <udf_ce@hotmail.com>
2023-06-02 21:18:31 -0600

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Agenda de  Pacientes
+        </h1>
+    )
+}
+
+export default Header;
